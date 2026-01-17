03832a18b03232cef2d891d6bb86a33e38726e70
Kanoel7 <nhvwjfjbx@gmail.com>
2026-01-17 15:28:56 +0100

    Fix GitHubPages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/SportsFacilityRental/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
